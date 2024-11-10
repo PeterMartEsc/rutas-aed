a3139b2d5ab51e73981bd9fa1fae9e5f010c3ee5
Nabil L.A. <nabil14716@gmail.com>
2024-11-10 14:28:22 +0000

    feat(models): Created all classes models

---
+++ routes-project/app/Models/Role.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $name
+ */
+class Role extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = ['id', 'name'];
+}
