d1118fdb9272cda4535425b2e387287d4b2a2d3d
Nabil L. A. <145138719+nalleon@users.noreply.github.com>
2024-11-16 16:23:00 +0000

    feat(script-bbdd): Updated sqlite script

--- script-bbdd/sqlite-version.sql
+++ script-bbdd/sqlite-version.sql
@@ -1,54 +1,42 @@
 DROP TABLE IF EXISTS `roles`;
-DROP TABLE IF EXISTS `groups`;
 DROP TABLE IF EXISTS `images`;
 DROP TABLE IF EXISTS `users`;
-DROP TABLE IF EXISTS `groups_users`;
 DROP TABLE IF EXISTS `routes`;
 
 CREATE TABLE `roles` (
-    `id` INTEGER PRIMARY KEY,
-    `name` TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE `groups` (
-    `id` INTEGER PRIMARY KEY,
-    `name` TEXT NOT NULL UNIQUE
+  `id` INTEGER PRIMARY KEY, 
+  `name` TEXT NOT NULL UNIQUE
 );
 
 CREATE TABLE `images` (
-    `id` INTEGER PRIMARY KEY,
-    `image` BLOB,
-    `type_image` TEXT
+  `id` INTEGER PRIMARY KEY, 
+  `image` BLOB,
+  `type_image` TEXT
 );
 
 CREATE TABLE `users` (
-    `id` INTEGER PRIMARY KEY,
-    `name` TEXT NOT NULL,
-    `surname` TEXT NOT NULL,
-    `email` TEXT NOT NULL UNIQUE,
-    `phone` TEXT NOT NULL,
-    `password` TEXT NOT NULL,
-    `id_image` INTEGER,
-    `id_role` INTEGER,
-    FOREIGN KEY (`id_image`) REFERENCES `images` (`id`),
-    FOREIGN KEY (`id_role`) REFERENCES `roles` (`id`)
-);
-
-CREATE TABLE `groups_users` (
-    `id` INTEGER PRIMARY KEY,
-    `id_group` INTEGER NOT NULL,
-    `id_user` INTEGER NOT NULL,
-    FOREIGN KEY (`id_group`) REFERENCES `groups` (`id`),
-    FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
+  `id` INTEGER PRIMARY KEY,
+  `name` TEXT NOT NULL,
+  `surname` TEXT NOT NULL,
+  `email` TEXT NOT NULL UNIQUE,
+  `phone` TEXT NOT NULL,
+  `password` TEXT NOT NULL,  
+  `id_image` INTEGER,
+  `id_role` INTEGER,
+  FOREIGN KEY (`id_image`) REFERENCES `images` (`id`),
+  FOREIGN KEY (`id_role`) REFERENCES `roles` (`id`)
 );
 
 CREATE TABLE `routes` (
-    `id` INTEGER PRIMARY KEY,
-    `title` TEXT UNIQUE,
-    `location` TEXT NOT NULL,
-    `distance` INTEGER,
-    `date_route` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    `description` TEXT,
-    `group_id` INTEGER,
-    FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`)
+  `id` INTEGER PRIMARY KEY,  
+  `title` TEXT UNIQUE,
+  `location` TEXT NOT NULL,
+  `distance` INTEGER,
+  `date_route` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `difficulty` INTEGER,
+  `pets_allowed` BOOLEAN DEFAULT FALSE,
+  `vehicle_needed` BOOLEAN DEFAULT FALSE,
+  `description` TEXT,  
+  `user_id` INTEGER,
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
