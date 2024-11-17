0200fcf48945c66219ad6a6fba364813af6d303c
Nabil L.A. <nabil14716@gmail.com>
2024-11-17 13:36:43 +0000

    style(login): Added basic bootstrap css to login

--- routes-project/resources/views/profileAdmin.blade.php
+++ routes-project/resources/views/profileAdmin.blade.php
@@ -32,12 +32,14 @@
     <!--Contenedor principal -->
     <div class="container">
         <div class="text-end">
-            @csrf
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
                 <x-dropdown-link :href="route('logout')"
                                 onclick="event.preventDefault(); this.closest('form').submit();">
                     {{ __('Log Out') }}
                 </x-dropdown-link>
-            <div>
+            </form>
+        </div>
         <div class="row">
             @auth
             <div class="col-6 ">
