b9e3b0634fcaf8bed85652c3eff529be3898b772
Nabil L.A. <nabil14716@gmail.com>
2024-11-20 00:05:41 +0000

    refactor(UserController): Removed duplicates from controller

--- routes-project/app/Http/Controllers/UserController.php
+++ routes-project/app/Http/Controllers/UserController.php
@@ -19,7 +19,6 @@ class UserController extends Controller
         $nextroute = $this->routeRepository->getNearestDateRouteByUser(auth()->user()->id);
         $followedroutes = $this->routeRepository->getRoutesOrderedByDate(auth()->user()->id);
         $createdroutes = $this->routeRepository->findRoutesCreatedByUserId(auth()->user()->id);
-
         return view('profile', compact('nextroute', 'followedroutes', 'createdroutes'));
     }
 
@@ -27,89 +26,6 @@ class UserController extends Controller
         return view('editUser');
     }
 
-    public function prepareRoutes(){
-        $routes = $this->routeRepository->findAll();
-        $nearestRouteByUser = $this->routeRepository->getNearestDateRouteByUser(auth()->user()->id);
-        $nearestRouteGlobally = $this->routeRepository->getNearestDateRouteGlobally();
-
-        $followedroutes = $this->routeRepository->getRoutesOrderedByDate(auth()->user()->id);
-        $routeIsInMyFollowing = false;
-        
-        foreach($followedroutes as $followedRoute){
-            if($followedRoute['id'] == $nearestRouteByUser['id']){
-                $routeIsInMyFollowing = true;
-                break;
-            }
-        }
-
-        return view('routes', compact('routes', 'nearestRouteByUser', 'nearestRouteGlobally', 'routeIsInMyFollowing'));
-    }
-
-
-    public function selectRoute(Request $request){
-        $selectedid = $request->route_id;
-        $selectedroute = $this->routeRepository->findById($selectedid);        
-
-        $routes = $this->routeRepository->findAll();
-        $followedroutes = $this->routeRepository->getRoutesOrderedByDate(auth()->user()->id);
-
-        $routeIsInMyFollowing = false;
-
-        foreach($followedroutes as $followedRoute){ 
-            if($followedRoute['id'] == $selectedroute['id']){
-                $routeIsInMyFollowing = true;
-                break;
-            }
-        }
-
-        return view('routes', compact('selectedroute', 'routes', 'followedroutes', 'routeIsInMyFollowing'));
-    }
-
-    public function signInForRoute(Request $request){
-        $userId = $request->input('userId');
-        $routeId = $request->input('routeId');
-        $isSigned = $this->routeRepository->signForRoute($userId, $routeId);
-
-        if (!$isSigned){
-            return redirect()->route('routes');
-        }
-
-        return redirect()->route('routes');
-
-    }
-
-
-    public function signOutForRoute(Request $request){
-        $userId = $request->input('userId');
-        $routeId = $request->input('routeId');
-
-        $isSigned = $this->routeRepository->signOutForRoute($userId, $routeId);
-
-        $aux = $this->routeRepository->getRoutesOrderedByDate($userId);
-
-        
-        if (!$isSigned){
-            return redirect()->route('routes');
-        }
-
-        return redirect()->route('routes');
-    }
-
-    /**
-     * Show the search form and results.
-     */
-    public function search(Request $request){
-        $filter = $request->input('filter', ''); 
-        $routes = [];
-
-        if (!empty($filter)) {
-            $routes = $this->routeRepository->filterRoutes($filter);
-        } else{
-            return redirect()->route('routes');
-        }
-
-        return view('routes', compact('routes', 'filter'));
-    }
 
 
 }
